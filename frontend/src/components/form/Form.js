89add012b7d3ba6e5280a1c1993a35343b11fa1d
Julia Morelli <jmlanghe@gmail.com>
2020-01-02 17:19:04 -0300

    Improve readibility in <Form />

--- frontend/src/components/form/Form.js
+++ frontend/src/components/form/Form.js
@@ -13,18 +13,26 @@ function Form({children, fields}) {
     return errs
   }, {}));
 
+  // This onChange method must be passed to each input within the form, called with the corresponding field key
   const onFieldChange = fieldKey => 
     event => {
+      // when an input field changes, we'll store its value in the form state
       let newValue = event.target.value;
       setValues({...values, [fieldKey]: newValue});
+
+      // and find its field to see if the new value is valid for the field's logic
       const field = fields.find(f => f.name === fieldKey);
+
       if (field.required && !newValue)
+        // there's no value and the field's required
         return setErrors({...errors, [fieldKey]: 'We need this info!'});
-      if (field.validate === undefined) 
+
+      if (!('validate' in field)) 
+        // there's no custom validation
         return setErrors({...errors, [fieldKey]: null});
-      const errMsg = field.validate(newValue);
-      if (errMsg)
-        return setErrors({...errors, [fieldKey]: errMsg});
+
+      // there's a custom validation (must return str/null)
+      return setErrors({...errors, [fieldKey]: field.validate(newValue)});
     };
 
   return (
